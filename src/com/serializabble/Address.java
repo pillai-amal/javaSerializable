64ca8df8cd67f0fe5de6be57f5391308e1b1bf58
pillai-amal <pillai_amal@hotmail.com>
2022-06-18 16:58:12 +0530

    inital commit

---
+++ src/com/serializabble/Address.java
@@ -0,0 +1,25 @@
+package com.serializabble;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+	private String city;
+	private int pincode;
+	public Address(String city, int pincode) {
+		super();
+		this.city = city;
+		this.pincode = pincode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+}
